8736fbe33145c07dc292db57ade3338ba5aaf4db
Medniy2000 <vvnikitiuk@gmail.com>
2018-07-08 23:34:39 +0300

    display meals with exceed table

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,22 +1,45 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.repository.InMemoryMealRepositoryImpl;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
+    private InMemoryMealRepositoryImpl repository = new InMemoryMealRepositoryImpl();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Meal> meals = new ArrayList<>(repository.getAll());
+
+        int caloriesPerDay = 2000;  // some value fo test
+
+        Map<LocalDate, Integer> caloriesSumByDate = meals.stream()
+                .collect(
+                        Collectors.groupingBy(Meal::getDate, Collectors.summingInt(Meal::getCalories))
+                );
+        List<MealWithExceed> mealWithExceeds = meals.stream().
+                map(m -> MealsUtil.createWithExceed(m, caloriesSumByDate.get(m.getDate()) > caloriesPerDay))
+                .collect(Collectors.toList());
+
         log.debug("redirect to meals");
-//      request.getRequestDispatcher("/meals.jsp").forward(request, response);
-        response.sendRedirect("meals.jsp");
+        request.setAttribute("mealsList",mealWithExceeds);
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+//        response.sendRedirect("meals.jsp");
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,10 +1,47 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib uri="http://sargue.net/jsptags/time" prefix="javatime" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <html>
 <head>
     <title>Meals</title>
+    <style type="text/css">
+        TABLE {
+            border-collapse: collapse;
+            border: 4px solid darkgray;
+        }
+        TD, TH {
+            padding: 5px;
+            border: 3px solid darkgray;
+            text-align: left;
+        }
+
+        tr.red{
+            background-color: red;
+        }
+
+        tr.green{
+            background-color:green;
+        }
+
+    </style>
 </head>
 <body>
 <h3><a href="/topjava">Home</a></h3>
 <h2>Meals</h2>
+<h2>Meal list</h2>
+<table>
+    <c:forEach items="${mealsList}" var="meal">
+        <c:set var="cleanedDateTime" value="${fn:replace(meal.dateTime, 'T', ' ')}" />
+        <fmt:parseDate value="${ cleanedDateTime }" pattern="yy-MM-dd HH:mm" var="parsedDateTime"/>
+
+        <tr class=${meal.exceed ? "red":"green"}>
+            <td>${parsedDateTime}</td>
+            <td>${meal.description}</td>
+            <td>${meal.calories}</td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
